5a54029672a69712b42c526dcacf3c9e75fb86b1
Hannes Kirsman <hannes.kirsman@wunder.io>
2021-09-28 10:49:54 +0300

    #62 Fix Drupal autoloader base path detection.

--- drupal-autoloader.php
+++ drupal-autoloader.php
@@ -2,5 +2,8 @@
 
 use Wunderio\GrumPHP\Drupal\DrupalAutoloader;
 
+$vendor_dir = dirname(dirname(dirname(__FILE__)));
+$base_dir = dirname($vendor_dir);
+
 $o = new DrupalAutoloader();
-$o->register('/app');
+$o->register($base_dir);
