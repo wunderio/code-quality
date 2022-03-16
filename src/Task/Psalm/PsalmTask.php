67318ba2f8b30cb93bbf3aedafcc1c207d7d18e7
Hannes Kirsman <hannes.kirsman@wunder.io>
2022-03-16 15:41:37 +0200

    #62 Force Psalm to 1 thead if pcntl or posix is missing.

--- src/Task/Psalm/PsalmTask.php
+++ src/Task/Psalm/PsalmTask.php
@@ -26,7 +26,14 @@ class PsalmTask extends AbstractMultiPathProcessingTask {
     $arguments->addOptionalArgument('--config=%s', $config['config']);
     $arguments->addOptionalArgument('--report=%s', $config['report']);
     $arguments->addOptionalArgument('--no-cache', $config['no_cache']);
-    $arguments->addOptionalArgument('--threads=%d', $config['threads']);
+    if (!extension_loaded('pcntl') || !extension_loaded('posix')) {
+      // Psalm will error if we don't have pcntl or posix installed as
+      // we have increased the threads in our default configs.
+      $arguments->addOptionalArgument('--threads=%d', 1);
+    }
+    else {
+      $arguments->addOptionalArgument('--threads=%d', $config['threads']);
+    }
     $arguments->add('--find-unused-code');
     $arguments->addOptionalBooleanArgument('--show-info=%s', $config['show_info'], 'true', 'false');
 
