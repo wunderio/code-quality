f5ebd8aea1116ce4d1179b7185ce577fa3407532
Hannes Kirsman <hannes.kirsman@wunder.io>
2023-03-31 16:06:13 +0300

    #91 Fix xdebug in Lando.

--- .lando/tooling-phpunit.sh
+++ .lando/tooling-phpunit.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/app/vendor/bin
 install -d /app/public/simpletest/browser_output
 cd /app/
-XDEBUG_MODE=coverage php \
+XDEBUG_MODE=debug php \
   /app/vendor/bin/phpunit \
   --configuration /app/phpunit.xml \
   "$@"
